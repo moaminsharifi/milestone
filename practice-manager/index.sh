fc8d43e96dd3eda0a2d98316f9dad2c336c6294a
Brian Salehi <salehibrian@gmail.com>
2020-11-21 20:09:07 +0330

    hierarchy new branch needs to refersh indexing

--- practice-manager/index.sh
+++ practice-manager/index.sh
@@ -16,6 +16,7 @@ function index_page {
 	local lower_entry=
 	local previous_bottom=
 	local previous_top=
+	local delimiter=
 	index=
 	next_index=
 	previous_index=
@@ -45,14 +46,22 @@ function index_page {
 			continue
 		fi
 
+		set -x
 		# always validate path step by step
 		if [ -e "$upper_hierarchy" ]
 		then
 			# check if there is an entry above current entry
 			upper_entry="$(grep -n -B1 "$entry" "${hierarchy%/*}/.index")"
-			upper_entry="${upper_entry#*:}"
+			if [ "$(echo "$upper_entry" | wc -l)" -gt 1 ]
+			then
+				delimiter="-"
+			else
+				delimiter=":"
+			fi
 			upper_entry="${upper_entry%$'\n'*}"
 			upper_entry="${upper_entry%% *}"
+			local_index="${upper_entry%%$delimiter*}"
+			upper_entry="${upper_entry#*$delimiter}"
 
 			# grep returns same entry name when there is no above entry
 			# so there is no above entry in the same depth
@@ -60,9 +69,22 @@ function index_page {
 			# in order to keep upper entry as close to entry as possible
 			if [ "$upper_entry" = "$entry" ]
 			then
-				previous_bottom="$(grep -B1 "${upper_entry}" "${upper_hierarchy}/.index")"
+				previous_bottom="$(grep -n -B1 "${upper_entry}" "${upper_hierarchy}/.index")"
+				if [ "$(echo "$previous_bottom" | wc -l)" -gt 1 ]
+				then
+					delimiter="-"
+				else
+					delimiter=":"
+				fi
 				previous_bottom="${previous_bottom%%$'\n'*}"
 				previous_bottom="${previous_bottom%% *}"
+				if [ -n "$previous_index" ]
+				then
+					previous_index="${previous_index}.${previous_bottom%%$delimiter*}"
+				else
+					previous_index="${previous_bottom%%$delimiter*}"
+				fi
+				previous_bottom="${previous_bottom#*$delimiter}"
 				upper_hierarchy="$upper_hierarchy/$previous_bottom"
 
 			# there is an entry above current entry in the same depth
@@ -71,6 +93,12 @@ function index_page {
 			# and makes new branch until next depth is analyzed
 			else
 				upper_hierarchy="${hierarchy%/*}/$upper_entry"
+				if [ -n "$previous_index" ]
+				then
+					previous_index="${previous_index}.${local_index%%:*}"
+				else
+					previous_index="${local_index%%:*}"
+				fi
 			fi
 		# fail safe, if upper hierarchy goes wrong, exit with an error
 		else
@@ -79,14 +107,24 @@ function index_page {
 			tput sgr0
 			return 1
 		fi
+		set +x
 
+		set -x
 		# always validate path step by step
 		if [ -e "$lower_hierarchy" ]
 		then
 			# check if there is an entry above current entry
-			lower_entry="$(grep -A1 "$entry" "${hierarchy%/*}/.index")"
+			lower_entry="$(grep -n -A1 "$entry" "${hierarchy%/*}/.index")"
+			if [ "$(echo "$lower_entry" | wc -l)" -gt 1 ]
+			then
+				delimiter="-"
+			else
+				delimiter=":"
+			fi
 			lower_entry="${lower_entry#*$'\n'}"
 			lower_entry="${lower_entry%% *}"
+			local_index="${lower_entry%%$delimiter*}"
+			lower_entry="${lower_entry#*$delimiter}"
 
 			# grep returns same entry name when there is no above entry
 			# so there is no above entry in the same depth
@@ -94,9 +132,22 @@ function index_page {
 			# in order to keep upper entry as close to entry as possible
 			if [ "$lower_entry" = "$entry" ]
 			then
-				previous_top="$(grep -A1 "${lower_entry}" "${lower_hierarchy}/.index")"
-				previous_top="${previous_top##*$'\n'}"
+				previous_top="$(grep -n -A1 "${lower_entry}" "${lower_hierarchy}/.index")"
+				if [ "$(echo "$previous_top" | wc -l)" -gt 1 ]
+				then
+					delimiter=":"
+				else
+					delimiter=":"
+				fi
+				previous_top="${previous_top%%$'\n'*}"
+				if [ -n "$next_index" ]
+				then
+					next_index="${next_index}.${previous_top%%$delimiter*}"
+				else
+					next_index="${previous_top%%$delimiter*}"
+				fi
 				previous_top="${previous_top%% *}"
+				previous_top="${previous_top#*$delimiter}"
 				lower_hierarchy="$lower_hierarchy/$previous_top"
 
 			# there is an entry above current entry in the same depth
@@ -105,6 +156,12 @@ function index_page {
 			# and makes new branch until next depth is analyzed
 			else
 				lower_hierarchy="${hierarchy%/*}/$lower_entry"
+				if [ -n "$next_index" ]
+				then
+					next_index="${next_index}.${local_index}"
+				else
+					next_index="${local_index}"
+				fi
 			fi
 		# fail safe, if upper hierarchy goes wrong, exit with an error
 		else
@@ -113,54 +170,7 @@ function index_page {
 			tput sgr0
 			return 1
 		fi
-
-		##### upper index
-		if [ -d "$upper_hierarchy" ]
-		then
-			if ! [ -f "${upper_hierarchy}/.index" ]
-			then
-				tput bold
-				echo "upper hierarchy not indexed: $upper_hierarchy" >&2
-				tput sgr0
-				return 1
-			fi
-		elif [ -f "$upper_hierarchy" ]
-		then
-			if [ "$entry" = "README.md" ]
-			then
-				previous_index="${previous_index}${zero_padding}0."
-				break
-			fi
-
-			index_holder="${upper_hierarchy%/*}/.index"
-			local_index="$(grep -c "$upper_entry" $index_holder)"
-			if [ "$local_index" -eq 0 ]
-			then
-				tput bold
-				echo "page not indexed in upper hierarchy: $index_holder" >&2
-				tput sgr0
-				return 3
-			elif [ "$local_index" -gt 1 ]
-			then
-				tput bold
-				echo "duplicate entry in upper hierarchy: $index_holder" >&2
-			fi
-			index_holder=
-			local_index=
-		else
-			tput bold
-			echo "broken upper hierarchy: $upper_hierarchy" >&2
-			tput sgr0
-			return 4
-		fi
-
-		match="$(grep -n "$upper_entry" "${upper_hierarchy%/*}/.index")"
-		match="${match%%:*}"
-		if [ "${#match}" -eq 1 ]
-		then
-			match="${zero_padding}${match}"
-		fi
-		previous_index="${previous_index}${match}."
+		set +x
 
 		###### index 
 		if [ -d "$hierarchy" ]
@@ -209,54 +219,6 @@ function index_page {
 			match="${zero_padding}${match}"
 		fi
 		index="${index}${match}."
-
-		##### lower index
-		if [ -d "$lower_hierarchy" ]
-		then
-			if ! [ -f "${lower_hierarchy}/.index" ]
-			then
-				tput bold
-				echo "lower hierarchy not indexed: $lower_hierarchy" >&2
-				tput sgr0
-				return 1
-			fi
-		elif [ -f "$lower_hierarchy" ]
-		then
-			if [ "$lower_entry" = "README.md" ]
-			then
-				next_index="${next_index}${zero_padding}0."
-				break
-			fi
-
-			index_holder="${lower_hierarchy%/*}/.index"
-			local_index="$(grep -c "$lower_entry" $index_holder)"
-			if [ "$local_index" -eq 0 ]
-			then
-				tput bold
-				echo "page not indexed in lower hierarchy: $index_holder" >&2
-				tput sgr0
-				return 3
-			elif [ "$local_index" -gt 1 ]
-			then
-				tput bold
-				echo "duplicate entry in lower hierarchy: $index_holder" >&2
-			fi
-			index_holder=
-			local_index=
-		else
-			tput bold
-			echo "broken lower hierarchy: $lower_hierarchy" >&2
-			tput sgr0
-			return 4
-		fi
-
-		match="$(grep -n "$lower_entry" "${lower_hierarchy%/*}/.index")"
-		match="${match%%:*}"
-		if [ "${#match}" -eq 1 ]
-		then
-			match="${zero_padding}${match}"
-		fi
-		next_index="${next_index}${match}."
 
 		((depth++))
 	done
